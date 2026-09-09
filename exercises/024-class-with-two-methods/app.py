f8c808fc43fa81b9c0e5bf2d222a778b69a37ee3
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-09 16:41:18 +0000

    Completar ejercicio 024: InputOutputString

--- exercises/024-class-with-two-methods/app.py
+++ exercises/024-class-with-two-methods/app.py
@@ -1,1 +1,13 @@
-# Your code here
+class InputOutputString:
+    def __init__(self):
+        self.string = ""
+
+    def get_string(self):
+        self.string = input()
+
+    def print_string(self):
+        print(self.string.upper())
+
+my_string = InputOutputString()
+my_string.get_string()
+my_string.print_string()
