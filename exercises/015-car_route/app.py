f01e25ded2d2dd9d311853ea38290618634ba0d8
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-09 15:35:31 +0000

    Completar ejercicio 015: car_route

--- exercises/015-car_route/app.py
+++ exercises/015-car_route/app.py
@@ -1,7 +1,8 @@
+import math
 # Complete the function to return the amount of days it will take to cover a route
 def car_route(n,m):
-  return None
+  return math.ceil(m / n)
 
 
 # Invoke the function with two integers
-print(car_route())
+print(car_route(20, 40))
