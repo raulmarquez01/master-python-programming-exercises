ee7b345ca2e6137739006ce136cbade6ddad5ffb
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-09 16:53:33 +0000

    Completar ejercicio 026: two_dimensional_list

--- exercises/026-two-dimensional-array/app.py
+++ exercises/026-two-dimensional-array/app.py
@@ -1,1 +1,10 @@
-# Your code here
+def two_dimensional_list(x, y):
+    result = []
+    for i in range(x):
+        row = []
+        for j in range(y):
+            row.append(i * j)
+        result.append(row)
+    print(result)
+
+two_dimensional_list(3, 5)
