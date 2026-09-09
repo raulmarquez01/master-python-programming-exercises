98c6809af074a929d01561bd83b648b6bbc2b9aa
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-09 15:44:34 +0000

    Completar ejercicio 017: total_cost

--- exercises/017-total_cost/app.py
+++ exercises/017-total_cost/app.py
@@ -1,6 +1,7 @@
 # Complete the function to return the total cost in dollars and cents of (n) cupcakes
 def total_cost(d, c, n):
-    return None
+    total_cents = (d * 100 + c) * n
+    return total_cents // 100, total_cents % 100
 
 
 # Invoke the function with three integers: total_cost(dollars, cents, number_of_cupcakes)
