d4e5b56c1074489dd4cf9afdb8a14cefcaffe862
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-10 15:49:32 +0000

    Completar ejercicio 030: divisible_binary

--- exercises/030-divisable-binary/app.py
+++ exercises/030-divisable-binary/app.py
@@ -1,1 +1,10 @@
-# Your code here
+def divisible_binary(s):
+    parts = s.split(",")
+    result = []
+    for p in parts:
+        decimal = int(p, 2)
+        if decimal % 5 == 0:
+            result.append(p)
+    print(",".join(result))
+
+divisible_binary("0100,0011,1010,1001")
