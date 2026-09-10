470c3a50a3378320752e5c6c288fda96e876abaa
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-10 17:09:37 +0000

    Completar ejercicio 044: static methods

--- exercises/044-static_methods/app.py
+++ exercises/044-static_methods/app.py
@@ -1,1 +1,9 @@
-# Your code here
+class MathOperations:
+    @staticmethod
+    def add_numbers(a, b):
+        return a + b
+
+
+math_operations_instance = MathOperations()
+sum_of_numbers = MathOperations.add_numbers(10, 15)
+print(f"Sum of Numbers: {sum_of_numbers}")
