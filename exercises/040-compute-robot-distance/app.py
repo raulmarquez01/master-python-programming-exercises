8966842274680f2b328e8a43a4ea82314444f580
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-10 16:54:39 +0000

    Completar ejercicio 040: compute robot distance

--- exercises/040-compute-robot-distance/app.py
+++ exercises/040-compute-robot-distance/app.py
@@ -1,1 +1,19 @@
-# Your code here
+import math
+
+def compute_robot_distance(movements):
+    x, y = 0, 0
+    for move in movements:
+        direction, steps = move.split()
+        steps = int(steps)
+        if direction == "UP":
+            y += steps
+        elif direction == "DOWN":
+            y -= steps
+        elif direction == "LEFT":
+            x -= steps
+        elif direction == "RIGHT":
+            x += steps
+    distance = math.sqrt(x ** 2 + y ** 2)
+    print(round(distance))
+
+compute_robot_distance(["UP 5", "DOWN 3", "LEFT 3", "RIGHT 2"])
