b633a9c5e26530d4e5205ea62e7422fc094c4b8d
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-10 16:40:08 +0000

    Completar ejercicio 037: valid_password

--- exercises/037-validity-of-password/app.py
+++ exercises/037-validity-of-password/app.py
@@ -1,1 +1,16 @@
-# Your code here
+import re
+
+def valid_password(password):
+    if len(password) < 6 or len(password) > 12:
+        return "Invalid password. Please try again"
+    if not re.search(r'[a-z]', password):
+        return "Invalid password. Please try again"
+    if not re.search(r'[0-9]', password):
+        return "Invalid password. Please try again"
+    if not re.search(r'[A-Z]', password):
+        return "Invalid password. Please try again"
+    if not re.search(r'[$#@]', password):
+        return "Invalid password. Please try again"
+    return "Valid password"
+
+print(valid_password("ABd1234@1"))
