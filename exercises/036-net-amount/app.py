d1a246f9c565b3811b4a9cbb47e16f624d004db0
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-10 16:36:12 +0000

    Completar ejercicio 036: net_amount

--- exercises/036-net-amount/app.py
+++ exercises/036-net-amount/app.py
@@ -1,1 +1,13 @@
-# Your code here
+def net_amount(s):
+    parts = s.split()
+    total = 0
+    for i in range(0, len(parts), 2):
+        op = parts[i]
+        amount = int(parts[i + 1])
+        if op == "D":
+            total += amount
+        elif op == "W":
+            total -= amount
+    return total
+
+print(net_amount("D 300 D 300 W 200 D 100"))
