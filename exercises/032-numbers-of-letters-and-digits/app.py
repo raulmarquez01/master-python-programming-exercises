0d29425353f9153100c2fc027482b758e68407f0
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-10 16:26:05 +0000

    Completar ejercicio 032: letters_and_digits

--- exercises/032-numbers-of-letters-and-digits/app.py
+++ exercises/032-numbers-of-letters-and-digits/app.py
@@ -1,1 +1,11 @@
-# Your code here
+def letters_and_digits(s):
+    counts = {"letters": 0, "digits": 0}
+    for c in s:
+        if c.isalpha():
+            counts["letters"] += 1
+        elif c.isdigit():
+            counts["digits"] += 1
+    print(f"LETTERS {counts['letters']}")
+    print(f"DIGITS {counts['digits']}")
+
+letters_and_digits("hello world! 123")
