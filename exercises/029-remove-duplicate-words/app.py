4552108a5f1067a8e14f96d3064af0bce85eb852
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-10 14:32:16 +0000

    Completar ejercicio 029: remove_duplicate_words

--- exercises/029-remove-duplicate-words/app.py
+++ exercises/029-remove-duplicate-words/app.py
@@ -1,1 +1,5 @@
-# Your code here
+def remove_duplicate_words(s):
+    words = set(s.split())
+    return " ".join(sorted(words))
+
+print(remove_duplicate_words("hello world and practice makes perfect and hello world again"))
