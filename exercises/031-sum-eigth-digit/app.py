af2b31afb3b8243c6de682f7c029eb0f6e7dce00
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-10 16:21:33 +0000

    Completar ejercicio 031: all_digits_even

--- exercises/031-sum-eigth-digit/app.py
+++ exercises/031-sum-eigth-digit/app.py
@@ -1,1 +1,8 @@
-# Your code here
+def all_digits_even():
+    result = []
+    for num in range(1000, 3001):
+        if all(int(d) % 2 == 0 for d in str(num)):
+            result.append(str(num))
+    print(",".join(result))
+
+all_digits_even()
