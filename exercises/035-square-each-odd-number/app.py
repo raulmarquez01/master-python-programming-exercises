089579afb61e9d10ac5f013f6968e8bce5d5f2c0
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-10 16:32:37 +0000

    Completar ejercicio 035: square_odd_numbers

--- exercises/035-square-each-odd-number/app.py
+++ exercises/035-square-each-odd-number/app.py
@@ -1,1 +1,6 @@
-# Your code here
+def square_odd_numbers(s):
+    nums = [int(n) for n in s.split(",")]
+    result = [n ** 2 for n in nums if n % 2 != 0]
+    return result
+
+print(square_odd_numbers("1,2,3,4,5,6,7,8,9"))
