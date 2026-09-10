02d392bf36a65f83d6c21c4bb0fa6390f73c9e81
raulmarquez01 <raulmarquezmata@gmail.com>
2026-09-10 17:12:22 +0000

    Completar ejercicio 045: class methods

--- exercises/045-class_methods/app.py
+++ exercises/045-class_methods/app.py
@@ -1,2 +1,12 @@
-# Your code here
+class MathOperations:
+    pi = 3.14159
 
+    @classmethod
+    def calculate_circle_area(cls, radius):
+        area = cls.pi * radius ** 2
+        return area
+
+
+circle_area = MathOperations.calculate_circle_area(5)
+
+print(f"Circle Area: {circle_area}")
